cf367fbf51723752f38b84725ba03d70e426f66d
Daniel McGraw <dan.j.mcgraw@gmail.com>
2021-10-19 09:56:45 -0600

    Add TimeUnit enum and constant values in nanoseconds

---
+++ src/constants/u64.rs
@@ -0,0 +1,29 @@
+/// Nanosecond = 1 Nanosecond
+pub const NANOSECOND: u64 = 1;
+
+/// 1 Microsecond = 1000 Nanoseconds
+pub const MICROSECOND: u64 = 1_000;
+
+/// 1 Milliseconds = 1e+6 Nanoseconds
+pub const MILLISECOND: u64 = 1_000_000;
+
+/// 1 Second = 1e+9 Nanoseconds
+pub const SECOND: u64 = 1_000_000_000;
+
+/// 1 Minute = 6e+10 Nanoseconds
+pub const MINUTE: u64 = 60_000_000_000;
+
+/// 1 Hour = 3.6e+12 Nanoseconds
+pub const HOUR: u64 = 3_600_000_000_000;
+
+/// 1 Day = 8.64e+13 Nanoseconds
+pub const DAY: u64 = 86_400_000_000_000;
+
+/// 1 Week = 6.048e+14 Nanoseconds
+pub const WEEK: u64 = 604_800_000_000_000;
+
+/// 1 Month = 2.628e+15 Nanoseconds
+pub const MONTH: u64 = 2_628_000_000_000_000;
+
+/// 1 Year = 3.154e+16 Nanosecondss
+pub const YEAR: u64 = 31_540_000_000_000_000;
